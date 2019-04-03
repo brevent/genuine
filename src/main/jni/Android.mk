d5fac86516bb28c4ace08d3c704c40e9296d4c71
Liu Dongmiao <liudongmiao@gmail.com>
2019-04-03 20:00:24 +0800

    fix build with GENUINE_NO_STL

--- src/main/jni/Android.mk
+++ src/main/jni/Android.mk
@@ -9,6 +9,7 @@ LOCAL_SRC_FILES += openat.c
 LOCAL_SRC_FILES += am-proxy.c pm.c
 LOCAL_SRC_FILES += anti-xposed.c apk-sign-v2.c
 LOCAL_SRC_FILES += epic.c libraries.cpp
+LOCAL_SRC_FILES += libraries-no-stl.c libraries-mock.cpp
 LOCAL_SRC_FILES += common.c handle-error.c native-activity.c bitmap.c
 LOCAL_LDLIBS    := -llog -landroid -ljnigraphics
 include $(BUILD_SHARED_LIBRARY)
